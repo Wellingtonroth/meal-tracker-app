c89bf6eabe25afaa327058f70934ad60aff927e6
Wellington Roth <wellingtonrothh@gmail.com>
2025-08-27 14:56:29 -0300

    feat: :sparkles: add fake icons, webmanifest and meta tags to identify pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,4 @@
+// nuxt.config.ts
 export default defineNuxtConfig({
   modules: ['@pinia/nuxt'],
   imports: {
@@ -8,7 +9,17 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: 'Meal Tracker',
-      meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1' }],
-    },
-  },
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'theme-color', content: '#0f172a' },
+        { name: 'apple-mobile-web-app-capable', content: 'yes' },
+        { name: 'apple-mobile-web-app-status-bar-style', content: 'default' },
+        { name: 'apple-mobile-web-app-title', content: 'Meal Tracker' }
+      ],
+      link: [
+        { rel: 'manifest', href: '/manifest.webmanifest' },
+        { rel: 'apple-touch-icon', href: '/icons/icon-192.png' }
+      ]
+    }
+  }
 })
