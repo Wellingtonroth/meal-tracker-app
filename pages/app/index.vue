e41275e45e558296294fc9f98c497b610bcce7fb
Wellington Roth <wellingtonrothh@gmail.com>
2025-12-16 17:10:22 -0300

    wip

---
+++ pages/app/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Dashboard</h1>
+    <p>Bem-vindo ao Meal Tracker!</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'app',
+  middleware: 'app',
+});
+</script>
---
+++ pages/app/plans/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <h1>Planos</h1>
+    <p>Lista de planos (placeholder).</p>
+  </section>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'app',
+  middleware: 'app',
+});
+</script>
