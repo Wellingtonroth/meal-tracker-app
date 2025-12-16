3871e9da549c3a0ffbf32f9f22255811ca186c00
Wellington Roth <wellingtonrothh@gmail.com>
2025-12-16 17:06:02 -0300

    wip

--- public/sw.js
+++ public/sw.js
@@ -1,31 +1,38 @@
-const CACHE = 'mealtracker-v1'
-const ASSETS = ['/', '/manifest.webmanifest']
+const CACHE = 'mealtracker-v1';
+const ASSETS = ['/', '/manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())
-  )
-})
+    caches
+      .open(CACHE)
+      .then((c) => c.addAll(ASSETS))
+      .then(() => self.skipWaiting()),
+  );
+});
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  )
-})
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim()),
+  );
+});
 
 self.addEventListener('fetch', (event) => {
-  const req = event.request
-  if (req.method !== 'GET') return
+  const req = event.request;
+  if (req.method !== 'GET') return;
   event.respondWith(
-    caches.match(req).then((cached) =>
-      cached ||
-      fetch(req).then((res) => {
-        const copy = res.clone()
-        caches.open(CACHE).then((c) => c.put(req, copy))
-        return res
-      }).catch(() => caches.match('/'))
-    )
-  )
-})
+    caches.match(req).then(
+      (cached) =>
+        cached ||
+        fetch(req)
+          .then((res) => {
+            const copy = res.clone();
+            caches.open(CACHE).then((c) => c.put(req, copy));
+            return res;
+          })
+          .catch(() => caches.match('/')),
+    ),
+  );
+});
